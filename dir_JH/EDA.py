fa542ad4543b0c36c950229bc166304bb269f549
jhchoi <chlwlghks08@naver.com>
2024-08-05 22:29:48 +0900

    [UPDATE] EDA코드 수정

--- dir_JH/EDA.py
+++ dir_JH/EDA.py
@@ -37,118 +37,127 @@ def read_excel_file(file_path: str, header: int = None) -> pd.DataFrame:
         return pd.read_csv(csv_file, low_memory=False)
 
 
-ROOT_DIR = "/storage/jhchoi/lgaimers_5/data"
-RANDOM_STATE = 110
 
-X_Dam = read_excel_file(os.path.join(ROOT_DIR, "Dam dispensing.xlsx"), header=1)
+def read_excel_file(file_path: str, header: int = None) -> pd.DataFrame:
+    csv_file = file_path.replace(".xlsx", ".csv")
 
-X_AutoClave = read_excel_file(
-    os.path.join(ROOT_DIR, "Auto clave.xlsx"), header=1
-)
+    if not os.path.exists(csv_file):
+        print("Converting excel to csv...")
+        if header:
+            df = pd.read_excel(file_path, header=header)
+        else:
+            df = pd.read_excel(file_path)
 
-X_Fill1 = read_excel_file(
-    os.path.join(ROOT_DIR, "Fill1 dispensing.xlsx"), header=1
-)
+        df.to_csv(csv_file, index=False)
+        print(f"  {file_path} -> {csv_file}")
+        return df
+    else:
+        print(f"  Reading {csv_file}")
+        return pd.read_csv(csv_file, low_memory=False)
 
-X_Fill2 = read_excel_file(
-    os.path.join(ROOT_DIR, "Fill2 dispensing.xlsx"), header=1
-)
 
-y = pd.read_csv(os.path.join(ROOT_DIR, "train_y.csv"))
+if __name__ == '__main__':
+    # ==== Configuration ====
+    ROOT_DIR = "/storage/jhchoi/lgaimers_5"
+    RANDOM_STATE = 110
+    # ========================
 
-# Rename columns
-X_Dam.columns = [i + " - Dam" for i in X_Dam.columns]
-X_AutoClave.columns = [i + " - AutoClave" for i in X_AutoClave.columns]
-X_Fill1.columns = [i + " - Fill1" for i in X_Fill1.columns]
-X_Fill2.columns = [i + " - Fill2" for i in X_Fill2.columns]
-X_Dam = X_Dam.rename(columns={"Set ID - Dam": "Set ID"})
-X_AutoClave = X_AutoClave.rename(columns={"Set ID - AutoClave": "Set ID"})
-X_Fill1 = X_Fill1.rename(columns={"Set ID - Fill1": "Set ID"})
-X_Fill2 = X_Fill2.rename(columns={"Set ID - Fill2": "Set ID"})
+    # ==== Read & Make dataset ====
+    X_Dam = read_excel_file(
+        os.path.join(ROOT_DIR, "data/Dam dispensing.xlsx"), header=1
+    )
+    X_AutoClave = read_excel_file(
+        os.path.join(ROOT_DIR, "data/Auto clave.xlsx"), header=1
+    )
+    X_Fill1 = read_excel_file(
+        os.path.join(ROOT_DIR, "data/Fill1 dispensing.xlsx"), header=1
+    )
+    X_Fill2 = read_excel_file(
+        os.path.join(ROOT_DIR, "data/Fill2 dispensing.xlsx"), header=1
+    )
+    # y   -> train       [Set ID: train ID, target: ground truth]
+    # sub -> submission  [Set ID: test ID,  target: fill with prediction]
+    y   = pd.read_csv(os.path.join(ROOT_DIR, "data/train_y.csv"))
+    sub = pd.read_csv(os.path.join(ROOT_DIR, "submission.csv"))
 
-# Merge X
-X = pd.merge(X_Dam, X_AutoClave, on="Set ID")
-X = pd.merge(X, X_Fill1, on="Set ID")
-X = pd.merge(X, X_Fill2, on="Set ID")
-X = X.drop(X[X.duplicated(subset="Set ID")].index).reset_index(drop=True)
+    # Rename columns
+    X_Dam.columns = [i + " - Dam" for i in X_Dam.columns]
+    X_AutoClave.columns = [i + " - AutoClave" for i in X_AutoClave.columns]
+    X_Fill1.columns = [i + " - Fill1" for i in X_Fill1.columns]
+    X_Fill2.columns = [i + " - Fill2" for i in X_Fill2.columns]
+    X_Dam = X_Dam.rename(columns={"Set ID - Dam": "Set ID"})
+    X_AutoClave = X_AutoClave.rename(columns={"Set ID - AutoClave": "Set ID"})
+    X_Fill1 = X_Fill1.rename(columns={"Set ID - Fill1": "Set ID"})
+    X_Fill2 = X_Fill2.rename(columns={"Set ID - Fill2": "Set ID"})
 
-# Merge X and y
-df_merged = pd.merge(X, y, "inner", on="Set ID")
+    # Merge X
+    X = pd.merge(X_Dam, X_AutoClave, on="Set ID")
+    X = pd.merge(X, X_Fill1, on="Set ID")
+    X = pd.merge(X, X_Fill2, on="Set ID")
+    X = X.drop(X[X.duplicated(subset="Set ID")].index).reset_index(drop=True)
+    # =======================
 
-# Drop columns with more than half of the values missing
-drop_cols = []
-for column in df_merged.columns:
-    if (df_merged[column].notnull().sum() // 2) < df_merged[
-        column
-    ].isnull().sum():
-        drop_cols.append(column)
-df_merged = df_merged.drop(drop_cols, axis=1)
+    # ==== Preprocessing ====
+    # Drop columns with more than half of the values missing
+    drop_cols = []
+    for column in X.columns:
+        if (X[column].notnull().sum() // 2) < X[column].isnull().sum():
+            drop_cols.append(column)
+    X = X.drop(drop_cols, axis=1)
 
-# Drop Lot ID
-df_merged = df_merged.drop("LOT ID - Dam", axis=1)
-df_merged = df_merged.drop("LOT ID - AutoClave", axis=1)
-df_merged = df_merged.drop("LOT ID - Fill1", axis=1)
-df_merged = df_merged.drop("LOT ID - Fill2", axis=1)
+    # Additional column drops
+    add_drop_cols = []
+    add_drop_cols += ["LOT ID - Dam", "LOT ID - AutoClave", "LOT ID - Fill1", "LOT ID - Fill2"]  # Lot ID
 
-features = df_merged.columns
+    X = X.drop(add_drop_cols, axis=1)
 
-features_dam = []  # 79
-features_calve = []  # 21
-features_fill1 = []  # 39
-features_fill2 = []  # 49
-features_other = []  # 2
-for f in features:
-    if f[-3:] == 'Dam':  # Dam features
-        features_dam.append(f)
-    elif f[-9:] == 'AutoClave':  # AutoClave features
-        features_calve.append(f)
-    elif f[-5:] == 'Fill1':  # Fill1 features
-        features_fill1.append(f)
-    elif f[-5:] == 'Fill2':  # Fill2 features
-        features_fill2.append(f)
-    else:  # Target and...
-        features_other.append(f)
+    # Merge X and y -> df_train, df_test
+    df_train = pd.merge(X, y, "inner", on="Set ID")
+    df_test  = pd.merge(X, sub, "inner", on="Set ID")
 
-feature_idx = 9
-print(
-    f"{features_dam[feature_idx]} // {features_calve[feature_idx]} // {features_fill1[feature_idx]} // {features_fill2[feature_idx]}")
+    # Label Encoder
+    feature_le = LabelEncoder()
+    target_le = LabelEncoder()
 
-# Column : model.suffix
-fs = ['Model.Suffix - Dam', 'Model.Suffix - AutoClave', 'Model.Suffix - Fill1', 'Model.Suffix - Fill2']
-df_melted = df_merged[fs].melt(var_name='Categories', value_name='Value')
-value_counts = df_melted.value_counts().reset_index(name='Frequency')
+    features_int = []
+    features_str = []
+    for col in df_train.columns:
+        try:
+            df_train[col] = df_train[col].astype(int)
+            features_int.append(col)
+        except:
+            if col == 'target':
+                continue
+                # df_train[col] = target_le.fit_transform(df_train[col])
+            else:
+                df_train[col] = feature_le.fit_transform(df_train[col])
+                features_str.append(col)
 
-fig, axs = plt.subplots(4, 1, figsize=(10, 15))
-for i, col in enumerate(fs):
-    df = value_counts[value_counts['Categories'] == col]
-    axs[i].bar(df['Value'], df['Frequency'])
+    # Time sorted by collect date of Dam
+    df_train = df_train.sort_values(by=["Collect Date - Dam"])
+    df_test  = df_test.sort_values(by=["Collect Date - Dam"])
 
-    for j, value in enumerate(df['Frequency'].values):
-        axs[i].text(value_counts.index[j], value, str(value), ha='center', va='bottom')
-plt.show()
-plt.close()
+    df_train_X, df_train_y = df_train[features_str + features_int], df_train['target']
+    df_test_X = df_test[features_str + features_int]
 
-# Label Encoder
-le = LabelEncoder()
+    # X features after preprocessing
+    all_features   = [col for col in df_train_X.columns]
+    dam_features   = [col for col in all_features if col[-3:] == "Dam"]
+    clave_features = [col for col in all_features if col[-5:] == "Clave"]
+    fill1_features = [col for col in all_features if col[-5:] == "Fill1"]
+    fill2_features = [col for col in all_features if col[-5:] == "Fill2"]
 
-features_int = []
-features_str = []
-for col in df_merged.columns:
-    try:
-        df_merged[col] = df_merged[col].astype(int)
-        features_int.append(col)
-    except:
-        df_merged[col] = le.fit_transform(df_merged[col])
-        features_str.append(col)
+    # correlation coefficient
+    plt.rcParams['figure.figsize'] = (50, 50)
+    df_look_heatmap = df_train_X[dam_features[:8]]  # 상관관계를 보고자 하는 data frame
+    sb.heatmap(df_look_heatmap.corr(),
+               annot_kws={
+                   'fontsize': 13,
+                   'fontweight': 'bold',
+                   'fontfamily': 'serif'
+               },
+               annot=True,
+               cmap='Greens',
+               vmin=-1, vmax=1)
 
-# correlation coefficient
-plt.rcParams['figure.figsize'] = (50, 50)
-sb.heatmap(df_merged.corr(),
-           annot_kws={
-               'fontsize': 13,
-               'fontweight': 'bold',
-               'fontfamily': 'serif'
-           },
-           annot=True,
-           cmap='Greens',
-           vmin=-1, vmax=1)
+
